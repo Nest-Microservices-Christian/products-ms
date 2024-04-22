cb0f4e60ba8a874ccbd70de537f8dd6773338b4e
Christian Sanchez <christianjsanchezr2025@gmail.com>
2024-04-22 14:58:20 -0400

    first commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import { envs } from './config';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port,
+      },
+    },
+  );
+  const logger = new Logger('Main');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen();
+  logger.log(`Application on port: ${envs.port}`);
+}
+bootstrap();
